ac0249e68901f4e2332ccc23c5e89a0a405472af
Félix Pereira <felixpereira538@gmail.com>
2022-08-07 23:33:29 +0200

    Create user reducer with redux

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,1 +1,27 @@
 
+import {createSlice} from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    loginStart: (state) => {
+      state.isFetching = true;
+    },
+    loginSuccess: (state, action) => {
+      state.isFetching = false;
+      state.user = action.payload;
+    },
+    loginFailure: (state, action) => {
+      state.isFetching = false;
+      state.error = true;
+    }
+  }
+});
+
+export const {loginStart, loginFail, loginSuccess} = userSlice.actions;
+export default userSlice.reducer;
